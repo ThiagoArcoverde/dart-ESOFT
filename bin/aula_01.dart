21efdd894b8271de2671abbfa3f7b46f0ae477fe
Thiago Arcoverde <tluiz2701@gmail.com>
2023-08-04 20:02:22 -0300

    initial commit

---
+++ bin/aula_01.dart
@@ -0,0 +1,5 @@
+import 'package:aula_01/aula_01.dart' as aula_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aula_01.calculate()}!');
+}
---
+++ test/aula_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_01/aula_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
