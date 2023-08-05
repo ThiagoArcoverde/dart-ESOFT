cb45c79243bb7a7dc86302c1f362db11036afa03
Thiago Arcoverde <tluiz2701@gmail.com>
2023-08-05 14:32:58 -0300

    Aula 01

--- bin/aula_01.dart
+++ bin/aula_01.dart
@@ -1,5 +1,60 @@
-import 'package:aula_01/aula_01.dart' as aula_01;
+// import 'package:aula_01/aula_01.dart' as aula_01;
+
+late String nome;
 
 void main(List<String> arguments) {
-  print('Hello world: ${aula_01.calculate()}!');
+  // var nome = 'Thiago';
+  // String outroNome = 'Arcoverde';
+
+  // dynamic tipoDinamico = "Soares";
+  // tipoDinamico = 1;
+
+  // print(nome);
+  // print(outroNome);
+  // print(tipoDinamico);
+
+
+  // String nome;
+
+  // String? nomeComNulo;
+
+  // print(nome);
+  // print(nomeComNulo);
+
+
+
+  // nome = _getNome();
+  // print(nome); 
+
+
+
+  // const listaCompras = ['Arroz', 'Feijão'];
+  // // listaCompras.add('Carne');
+
+  // final listaItens = ['Monitor', 'Teclado'];
+  // listaItens.add('Computador');
+
+  // print(listaCompras);  
+  // print(listaItens);
+
+
+
+  // List<String> listaCompras = ["Arroz"];
+
+  // print(listaCompras);
+
+
+
+  // Set<String> listaCompras = {"Arroz"};
+
+  // listaCompras.add("Arroz");
+
+  // print (listaCompras);
+
+
+  print(42);
 }
+
+// String _getNome(){
+//   return "";
+// }
